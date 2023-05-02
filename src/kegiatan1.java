d957be56ab782f4bf2e8026548521183ba88c335
anantapir <115287802+Ananta-Pirdhaus@users.noreply.github.com>
2023-05-02 10:45:50 +0700

    Initial commit

---
+++ src/kegiatan1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan string: ");
+        String inputStr = input.nextLine();
+
+        String outputStr = reverseString(inputStr);
+        System.out.println("Hasil pembalikan string: " + outputStr);
+    }
+
+    public static String reverseString(String inputStr) {
+        // membuat stack baru
+        char[] stack = new char[inputStr.length()];
+
+        // memasukkan setiap karakter string ke dalam stack
+        for (int i = 0; i < inputStr.length(); i++) {
+            stack[i] = inputStr.charAt(i);
+        }
+
+        // membuat string baru dari karakter-karakter stack
+        String outputStr = "";
+        for (int i = inputStr.length() - 1; i >= 0; i--) {
+            outputStr += stack[i];
+        }
+
+        return outputStr;
+    }
+}
---
+++ src/kegiatan2.java
@@ -0,0 +1,6 @@
+package PACKAGE_NAME;/**
+    * @created 09/04/2023 - 21:49
+    * @project Modul3
+    * @author anant
+*/public class kegiatan2 {
+}
